4b55a357508459bb421d92e1fc1741868ec744b5
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-14 19:59:14 +0300

    day 38 :Inheritance-Created Car Class

---
+++ src/day38_Inheritance/Car/Car.java
@@ -0,0 +1,32 @@
+package day38_Inheritance.Car;
+
+public class Car {
+    public static void main(String[] args) {
+        
+    }
+}
+/*
+	carTask:
+		Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+
+				add a constructor to set all the fields
+
+			instance methods:
+				start(), drive(),toString()
+
+		Create the following sub classes of Car:
+			1. Toyota:
+					extra methods:
+							reliabile()
+
+			2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+
+			3. Tesla:
+					extra methods:
+						autoPilot();
+ */
