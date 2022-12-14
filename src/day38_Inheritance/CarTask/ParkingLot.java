92a62b716b225c3d6225fc35fe277f22ec3a9347
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-14 21:52:26 +0300

    day 38 :Inheritance- created override method start()

--- src/day38_Inheritance/CarTask/ParkingLot.java
+++ src/day38_Inheritance/CarTask/ParkingLot.java
@@ -6,6 +6,10 @@ public class ParkingLot {
         Toyota toyota = new Toyota("Camry",2020,20000,"Gray",123456);
         Tesla tesla = new Tesla("Model S",2022,94990,"Red",0);
         BMW bmw = new BMW("X5",2021,40000,"White",10000);
+
+        toyota.start();
+        tesla.start();
+        bmw.start();
     }
 
 }
