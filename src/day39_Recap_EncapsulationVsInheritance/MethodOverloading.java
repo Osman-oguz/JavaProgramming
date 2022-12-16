382b42f311288d9ca42d225a51099ca03a54999c
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-16 20:24:06 +0300

    day 39 : created Shape.java

---
+++ src/day39_Recap_EncapsulationVsInheritance/MethodOverloading.java
@@ -0,0 +1,10 @@
+package day39_Recap_EncapsulationVsInheritance;
+
+public class MethodOverloading {
+    public void method1(){
+
+    }
+    private int method1(int a){
+        return a;
+    }
+}
