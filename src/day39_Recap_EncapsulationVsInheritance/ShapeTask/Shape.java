8059fc93e87c72b41d26b09e46e9d9938b62ca08
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-16 20:31:13 +0300

    day 39 : added to string() to the  Shape.java

--- src/day39_Recap_EncapsulationVsInheritance/ShapeTask/Shape.java
+++ src/day39_Recap_EncapsulationVsInheritance/ShapeTask/Shape.java
@@ -28,4 +28,13 @@ public class Shape {
     public double perimeter(){
         return 0;
     }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
