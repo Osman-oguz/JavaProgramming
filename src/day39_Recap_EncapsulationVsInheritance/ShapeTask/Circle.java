63186ef476fa782768aa78a67a65d0fce0a8f7ef
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-17 11:35:33 +0300

    day 39 : updated  Circle.java class

--- src/day39_Recap_EncapsulationVsInheritance/ShapeTask/Circle.java
+++ src/day39_Recap_EncapsulationVsInheritance/ShapeTask/Circle.java
@@ -21,4 +21,22 @@ public class Circle extends Shape {
         setRadius(radius);
     }
 
+    @Override
+    public double area() {
+        return radius * radius * pi;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * radius * pi;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
