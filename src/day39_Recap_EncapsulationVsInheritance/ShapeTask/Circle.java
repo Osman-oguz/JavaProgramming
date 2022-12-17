f290dc27a2054dee6aa33f4f0c3de9663bf0ce4d
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-17 11:33:05 +0300

    day 39 : created  Circle.java class

---
+++ src/day39_Recap_EncapsulationVsInheritance/ShapeTask/Circle.java
@@ -0,0 +1,24 @@
+package day39_Recap_EncapsulationVsInheritance.ShapeTask;
+
+public class Circle extends Shape {
+    private double radius;
+    public static double pi = 3.14;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if( radius <= 0){
+            System.err.println("Invalid radius: " + radius);
+            System.exit(1);
+        }
+        this.radius = radius;
+    }
+
+    public Circle( double radius)  {
+        super("Circle");
+        setRadius(radius);
+    }
+
+}
