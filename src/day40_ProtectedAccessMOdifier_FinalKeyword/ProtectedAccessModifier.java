284bc96fd1e3d588db9a0d7f741404ec7f4704bc
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-21 20:03:21 +0300

    created day40_ProtectedAccessModifier

---
+++ src/day40_ProtectedAccessMOdifier_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,16 @@
+package day40_ProtectedAccessMOdifier_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1= "Cydeo"; // default access modifier
+    protected static String name2= "Wooden Spoon";
+
+    static void method1(){
+
+    }
+    protected static void methods2(){
+
+    }
+
+
+}
