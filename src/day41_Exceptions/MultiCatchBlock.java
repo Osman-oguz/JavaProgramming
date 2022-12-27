572df45007267a1a4b77f21e7089c3f41656eca0
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-27 20:04:28 +0300

    created day41_MultiCatchBlock

---
+++ src/day41_Exceptions/MultiCatchBlock.java
@@ -0,0 +1,38 @@
+package day41_Exceptions;
+
+import day39_Recap_EncapsulationVsInheritance.cydeoTask.Employee;
+
+public class MultiCatchBlock {
+
+    public static void main(String[] args) {
+        Employee employee = null;
+
+
+        try {
+            System.out.println(employee.getSalary());
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("First catch block");
+            e.printStackTrace();
+        }catch (ArithmeticException e){
+            System.out.println("Second catch block");
+            e.printStackTrace();
+        }catch (ClassCastException e){
+            System.out.println("Third catch block");
+            e.printStackTrace();
+        }catch (RuntimeException e){
+            System.out.println("Fourth catch block");
+            e.printStackTrace();
+        }
+        System.out.println("Test completed");
+
+        System.out.println("-----------------------------------");
+
+        try {
+            System.out.println("Java".charAt(-1));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
