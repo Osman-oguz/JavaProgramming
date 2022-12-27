00473ca4665367e2c9a01dd03a9908360d0fa7dc
Osman Oğuz <osman-oguz@hotmail.com>
2022-12-27 23:57:02 +0300

    created day42_ThrowsKeyword

---
+++ src/day42_Exceptions/ThrowsKeyword.java
@@ -0,0 +1,30 @@
+package day42_Exceptions;
+
+import java.nio.file.FileAlreadyExistsException;
+
+public class ThrowsKeyword {
+
+    public static void main(String[] args) throws InterruptedException{
+
+        System.out.println("-----------Test1------------");
+
+        System.out.println("Hello");
+
+        Thread.sleep(3000);
+
+        System.out.println("Cydeo");
+        System.out.println("----------Test1 Completed---------");
+
+        System.out.println("-----------Test2------------");
+
+        System.out.println("Hello");
+
+        Thread.sleep(3000);
+
+        System.out.println("EU10");
+        System.out.println("----------Test2 Completed---------");
+
+
+
+    }
+}
